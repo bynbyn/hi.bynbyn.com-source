3eeb435e0ec115f7dc7a56ec2ce906245acb5556
孟政元 <278227739@qq.com>
2017-10-22 23:20:10 +0800

    配置更新

--- pm2.js
+++ pm2.js
@@ -6,14 +6,14 @@ const server = spawn('hexo', ['server']);
 server.stdout.on('data', function (data)
 {
 
-    console.log('standard output:\n' + data);
+    console.log(data.toString());
 
 });
 
 server.stderr.on('data', function (data)
 {
 
-    console.log('standard error output:\n' + data);
+    console.error(data.toString());
 
 });
 
@@ -21,11 +21,9 @@ server.on('exit', function (code, signal)
 {
 
     console.log('child process eixt ,exit:' + code);
-    process.exit(-1);
 
 });
 
 process.on('exit', function()
 {
-    server.exit();
 });
